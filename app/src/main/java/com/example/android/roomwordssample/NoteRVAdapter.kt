20bdf2d335ead28f87f695bc29d746ed8e5343dd
sumanAndroidMaxmaven <suman@maxmaven-solutions.com>
2023-07-20 22:15:09 +0530

    inial 5

--- app/src/main/java/com/example/android/roomwordssample/NoteRVAdapter.kt
+++ app/src/main/java/com/example/android/roomwordssample/NoteRVAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.android.roomwordssample
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 class NoteRVAdapter(
     val context: Context,
@@ -26,6 +33,7 @@ class NoteRVAdapter(
         val noteTV = itemView.findViewById<TextView>(R.id.idTVNote)
         val dateTV = itemView.findViewById<TextView>(R.id.idTVDate)
         val deleteIV = itemView.findViewById<ImageView>(R.id.idIVDelete)
+        val vwColorBtm = itemView.findViewById<View>(R.id.vwColorBtm)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -37,8 +45,24 @@ class NoteRVAdapter(
         return ViewHolder(itemView)
     }
 
+
+
+
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // on below line we are setting data to item of recycler view.
+
+        val currentItem = allNotes[position]
+
+        /*val drawable = holder.vwColorBtm.background
+        val bitmap = drawableToBitmap(drawable)
+        val color = getPaletteGeneratedColor(bitmap)
+        holder.vwColorBtm.setBackgroundColor(color)
+*/
+        // Set a random background color to the item view
+        val randomColor = getRandomColor()
+        holder.vwColorBtm.setBackgroundColor(randomColor)
+
         holder.noteTV.setText(allNotes.get(position).noteTitle)
         holder.dateTV.setText("Last Updated : " + allNotes.get(position).timeStamp)
         // on below line we are adding click listener to our delete image view icon.
@@ -87,4 +111,40 @@ interface NoteClickInterface {
     // creating a method for click action
     // on recycler view item for updating it.
     fun onNoteClick(note: Note)
+}
+
+
+
+private fun getPaletteGeneratedColor(bitmap: Bitmap): Int {
+    val palette = Palette.from(bitmap).generate()
+    val vibrantSwatch = palette.vibrantSwatch ?: palette.dominantSwatch
+    return vibrantSwatch?.rgb ?: getRandomColor()
+}
+
+// Function to convert a Drawable to Bitmap
+private fun drawableToBitmap(drawable: Drawable): Bitmap {
+    if (drawable is BitmapDrawable) {
+        return drawable.bitmap
+    }
+
+    val width = drawable.intrinsicWidth.takeIf { it > 0 } ?: 1
+    val height = drawable.intrinsicHeight.takeIf { it > 0 } ?: 1
+
+    val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+
+    return bitmap
+}
+
+
+private fun getRandomColor(): Int {
+    // Generate random RGB values
+    val r = Random.nextInt(256)
+    val g = Random.nextInt(256)
+    val b = Random.nextInt(256)
+
+    // Create a color from the RGB values
+    return Color.rgb(r, g, b)
 }
